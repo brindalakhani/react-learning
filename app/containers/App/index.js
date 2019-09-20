316213edee4f140afbda65e1bf5f0228a5af3be6
brinda.l <brinda.l@innovify.in>
2019-09-20 12:24:51 +0530

    added bootstrap css

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,7 +10,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import TodoApp from 'containers/TodoApp/Loadable';
